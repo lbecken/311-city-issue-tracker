4607d83a9ec24a2d679f173173ea13850931788d
Luis Becken <lbecken@gmail.com>
2025-11-08 22:39:38 +0100

    create phase1/foundation

---
+++ mw-citizen-tracker/src/main/java/gov/lby/mw_citizen_tracker/MwCitizenTrackerApplication.java
@@ -0,0 +1,13 @@
+package gov.lby.mw_citizen_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MwCitizenTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MwCitizenTrackerApplication.class, args);
+	}
+
+}
